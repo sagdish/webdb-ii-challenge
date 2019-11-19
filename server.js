c3da0759f39de0c01e64a04deaa3f83ac6a022d5
sagdish <forsi1@yahoo.com>
2019-11-19 15:11:08 +0500

    mvp completed

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const db = require('./data/db-config.js');
 
 const server = express();
 
 server.use(express.json());
 
 // routes go here:
+
+server.get('/inventory', (req, res) => {
+  db('cars')
+    .then(inventory => {
+      res.status(200).json(inventory);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/inventory', (req, res) => {
+  const car = req.body;
+  
+  if (make || model)
+
+  db('cars')
+    .then(inventory => {
+      res.status(200).json(inventory);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = server;
