966e6ca0128d36527f7045d17595d1b6f8c1ff8e
sagdish <forsi1@yahoo.com>
2019-11-20 15:03:07 +0500

    post method fix, proper insertions to the database with properties checking before insertion to db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -5,9 +5,21 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('cars').insert([
-        {make: 'Toyota', model: 'Prius', VIN: 'JH4KA3170LC006787', mileage: 154999, title: 'rebuilt', transmission: 'automatic'},
-        {make: 'Volvo', model: 'S60', VIN: 'JH4KA3170LC006754', mileage: 84231, title: 'clean', transmission: 'automatic', color: 'gold'},
-        {make: 'Lada', model: '2106', VIN: 'SCAZD00A6MCX30611', mileage: 63256, title: 'clean', transmission: 'manual', color: 'blue'}
+        {
+          make: 'Toyota', model: 'Prius', 
+          VIN: 'JH4KA3170LC006787', mileage: 154999, 
+          title: 'rebuilt', transmission: 'automatic'
+        },
+        {
+          make: 'Volvo', model: 'S60',
+          VIN: 'JH4KA3170LC006754', mileage: 84231,
+          title: 'clean', transmission: 'automatic', color: 'gold'
+        },
+        {
+          make: 'Lada', model: '2106',
+          VIN: 'SCAZD00A6MCX30611', mileage: 63256,
+          title: 'clean', transmission: 'manual', color: 'blue'
+        }
       ]);
     });
 };
--- server.js
+++ server.js
@@ -19,13 +19,25 @@ server.get('/inventory', (req, res) => {
 server.post('/inventory', (req, res) => {
   const car = req.body;
   
+  const { make, model, VIN, mileage } = car;
   if (!make || !model || !VIN || !mileage) {
     res.status(404).json({ error: "please specify required fields" });
+    return;
   }
 
-  db('cars')
+  db('cars').insert(car)
     .then(inventory => {
       res.status(200).json(inventory);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get('/inventory/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars').where({id}).first()
+    .then(car => {
+      res.status(200).json(car);
     })
     .catch(err => res.status(500).json(err));
 });
